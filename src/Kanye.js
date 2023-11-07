a99de1322eab84291975dcf47a655c6259f4d4f4
anthonybacon916@gmail.com <anthonybacon916@gmail.com>
2023-11-07 22:24:42 +0000

    New Try

--- src/Kanye.js
+++ src/Kanye.js
@@ -1,10 +1,13 @@
+import './Kanye.css';
 import React from "react";
 
 function Kanye(props) {
     const search = () => {props.search()};
 
     return (
-        <button onClick={props.search}><img src="https://parade.com/.image/t_share/MTkzMjc5NDI3MTg5MTU1NzQ3/kanye-west-net-worth.jpg"/></button>
+        <div className='buttonDiv'>
+            <button onClick={props.search}><img src="https://parade.com/.image/t_share/MTkzMjc5NDI3MTg5MTU1NzQ3/kanye-west-net-worth.jpg"/></button>
+        </div>
     );
 }
 
